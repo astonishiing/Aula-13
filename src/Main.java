a32f8a4c2f4d02fa5dc94ca88fdbc068273f45d1
astonishiing <wcardoso4510@gmail.com>
2025-02-18 15:44:32 -0300

    criação de sobrecarga com a classe Cachorro e teste

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         //Programa principal
         Lobo x = new Lobo();
+        Cachorro c = new Cachorro();
         x.emitirSom();
+        c.emitirSom();
     }
 }
